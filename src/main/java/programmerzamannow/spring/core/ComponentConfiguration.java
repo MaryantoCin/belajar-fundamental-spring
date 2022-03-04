f95ed6705065fb6a369778e627752434a779a7b5
Maryanto Cin <firevolt719@gmail.com>
2022-03-04 10:16:25 +0700

    Constructor-based DI

--- src/main/java/programmerzamannow/spring/core/ComponentConfiguration.java
+++ src/main/java/programmerzamannow/spring/core/ComponentConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan(basePackages = {
+        "programmerzamannow.spring.core.repository",
         "programmerzamannow.spring.core.service"
 })
 public class ComponentConfiguration {
